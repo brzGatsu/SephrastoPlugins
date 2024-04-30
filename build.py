1cf6f43864a338cd976c3e58d1af291cc93f7a06
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2024-04-30 13:39:01 +0200

    bump version

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-version = "5.0.0"
+version = "5.0.1"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 build_path = os.path.join(dir_path,  "build")
 os.makedirs(build_path)
