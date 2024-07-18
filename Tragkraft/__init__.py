bacb293e0d964b44b4cb56fa2444c24591244ec7
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2024-07-18 14:59:13 +0200

    Tragkraft: fixed last not being added to the correct cell in the PDF; changed default Tragkraft formula to reflect rule changes; removed be calculation support; not locking first cell anymore

--- Tragkraft/__init__.py
+++ Tragkraft/__init__.py
@@ -9,7 +9,6 @@ class Plugin:
     def __init__(self):
         EventBus.addAction("basisdatenbank_geladen", self.basisDatenbankGeladenHandler)
         EventBus.addAction("datenbank_geladen", self.datenbankGeladenHandler)
-        EventBus.addAction("post_charakter_aktualisieren", self.postCharakterAktualisierenHandler)
         EventBus.addAction("charakter_instanziiert", self.charakterInstanziiertHandler)
         EventBus.addFilter("class_inventar_wrapper", self.provideInventarWrapperHook)
         EventBus.addFilter("pdf_export", self.pdfExportHook)
@@ -32,20 +31,12 @@ class Plugin:
         ab = AbgeleiteterWertDefinition()
         ab.name = "TK"
         ab.anzeigename = "Tragkraft"
-        ab.text = "Die Tragkraft bestimmt, wieviel Last du mit dir führen kannst. Jeder getragene Gegenstand, der über diese Menge hinausgeht, erhöht deine BE um 1, reduziert dein DH also um 2. Fällt dein DH dadurch auf 1, kannst du keinen weiteren Gegenstand transportieren."
-        ab.formel = "KK * 2"
-        ab.script = "getAttribut('KK')*2"
+        ab.text = "Du kannst Gegenstände mit einer Last von bis zu deiner Tragkraft über weite Strecken transportieren. Sie beträgt wenigstens 2."
+        ab.formel = "KK + KO"
+        ab.script = "max(getAttribut('KK') + getAttribut('KO'), 2)"
         ab.finalscript = ""
         ab.sortorder = 60
         self.db.loadElement(ab)
-
-        e = DatenbankEinstellung()
-        e.name = "Tragkraft Plugin: Last BE Script"
-        e.beschreibung = "Das Pythonscript berechnet den BE-Modifikator durch Tragkraft und Last. Es stehen hierfür die Funktionen getAttribut('Attributsname'), getTK() und getLast() zur Verfügung.\n" +\
-           "Letztere liefert einen Int-Array mit den Last-Einträgen der Inventarzeilen zurück (exklusive der ersten Zeile). Der Modifikator muss der Variable 'be' zugewiesen werden."
-        e.text = "be = max(sum(getLast()) - getTK(), 0)"
-        e.typ = "Exec"
-        self.db.loadElement(e)
 
     def datenbankGeladenHandler(self, params):
         if not self.db.einstellungen["Tragkraft Plugin: Aktivieren"].wert and params["isCharakterEditor"]:
@@ -59,55 +50,18 @@ class Plugin:
         char = params["charakter"]
         char.ausrüstungPlatzbedarf = []
 
-    def postCharakterAktualisierenHandler(self, params):
-        if not self.db.einstellungen["Tragkraft Plugin: Aktivieren"].wert:
-            return
-        char = params["charakter"]
-
-        pb = 0
-        if len(char.ausrüstungPlatzbedarf) > 0:
-            pb = char.ausrüstungPlatzbedarf[0]
-
-        scriptAPI = Hilfsmethoden.createScriptAPI()
-        scriptAPI.update({
-            'getAttribut' : lambda attribut: char.attribute[attribut].wert,
-            'getTK' : lambda: char.abgeleiteteWerte["TK"].wert,
-            'getLast' : lambda: char.ausrüstungPlatzbedarf[1:] if len(char.ausrüstungPlatzbedarf) > 1 else [0],
-            'be' : 0
-        })
-        Wolke.DB.einstellungen["Tragkraft Plugin: Last BE Script"].executeScript(scriptAPI)
-        text = f"Tragkraft {char.abgeleiteteWerte['TK'].wert}, Last {pb} -> zusätzliche BE {scriptAPI['be']}"
-
-        if len(char.ausrüstung) == 0:
-            char.ausrüstung.append(text)
-        elif char.ausrüstung[0].startswith("Tragkraft"):
-            char.ausrüstung[0] = text
-        else:
-            char.ausrüstung.insert(0, text)
-
-        if len(char.ausrüstungPlatzbedarf) == 0:
-            char.ausrüstungPlatzbedarf.append(0)
-
     def pdfExportHook(self, fields, params):
         if not self.db.einstellungen["Tragkraft Plugin: Aktivieren"].wert:
             return fields
-        count = 1
-        countl = 1
-        countr = 11
+
         for i in range(len(Wolke.Char.ausrüstung)):
-            if count % 2 != 0:
-                index = countl
-                countl += 1
-            else:
-                index = countr
-                countr += 1
-            if i == 0:
-                pass
-            elif len(Wolke.Char.ausrüstungPlatzbedarf) > i and Wolke.Char.ausrüstungPlatzbedarf[i] > 0:
-                fields['Ausruestung' + str(index)] += " (Last " + str(Wolke.Char.ausrüstungPlatzbedarf[i]) + ")"
-            if count >= 20:
+            field = 'Ausruestung' + str(i+1)
+            if field not in fields or not fields[field].strip():
+                continue
+            if len(Wolke.Char.ausrüstungPlatzbedarf) <= i:
                 break
-            count += 1
+            fields[field] += " (Last " + str(Wolke.Char.ausrüstungPlatzbedarf[i]) + ")"
+
         return fields
 
     
@@ -152,23 +106,15 @@ class Plugin:
 
                 rowCount = 0
                 colCount = 0
-                self.leTK = None
                 
                 for i in range(20):
                     lineEdit = self.ui.gridLayout_2.itemAtPosition(rowCount, colCount).widget()
                     spin = QtWidgets.QSpinBox()
                     spin.setMinimum(0)
                     spin.setAlignment(QtCore.Qt.AlignCenter)
-                    if i == 0:
-                        self.leTK = lineEdit
-                        spin.setReadOnly(True)
-                        spin.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
-                        lineEdit.setReadOnly(True)
-                        spin.setMaximum(999)
-                    else:
-                        spin.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
-                        spin.valueChanged.connect(self.updatePlatzbedarf)
-                        spin.setMaximum(32)
+                    spin.setButtonSymbols(QtWidgets.QAbstractSpinBox.PlusMinus)
+                    spin.valueChanged.connect(self.updatePlatzbedarf)
+                    spin.setMaximum(99)
                     self.ui.gridLayout_2.addWidget(lineEdit, rowCount + 1, 0 if colCount == 0 else 2, 1, 1)
                     self.ui.gridLayout_2.addWidget(spin, rowCount + 1, 1 if colCount == 0 else 3, 1, 1)
                     self.tkSpin.append(spin)
@@ -206,14 +152,10 @@ class Plugin:
                 if self.currentlyLoading:
                     return
                 Wolke.Char.ausrüstungPlatzbedarf = []
-                for spin in self.tkSpin[1:]:
+                for spin in self.tkSpin:
                     Wolke.Char.ausrüstungPlatzbedarf.append(spin.value())
-                self.tkSpin[0].setValue(sum(Wolke.Char.ausrüstungPlatzbedarf))
-                Wolke.Char.ausrüstungPlatzbedarf.insert(0, self.tkSpin[0].value())
 
                 self.modified.emit()
-                if len(Wolke.Char.ausrüstung) > 0:
-                    self.leTK.setText(Wolke.Char.ausrüstung[0])
 
             def updateInventory(self):
                 super().updateInventory()
@@ -222,7 +164,7 @@ class Plugin:
                     Wolke.Char.ausrüstungPlatzbedarf.append(0)
 
             def updateTKSpinner(self):
-                for i in range(1,20):
+                for i in range(20):
                     if i < len(Wolke.Char.ausrüstung) and Wolke.Char.ausrüstung[i]:
                         self.tkSpin[i].setEnabled(True)
                     else:
