550cf02d29f47eb0d535d050234bafe00eba813c
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2024-11-30 22:46:15 +0100

    RüstungenPlus: place rüstung tabs in a scroll area

--- RuestungenPlus/RSCharakterRuestung.py
+++ RuestungenPlus/RSCharakterRuestung.py
@@ -17,8 +17,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QCheckBox, QGridLayout,
     QGroupBox, QLabel, QLayout, QLineEdit,
-    QSizePolicy, QSpacerItem, QSpinBox, QVBoxLayout,
-    QWidget)
+    QScrollArea, QSizePolicy, QSpacerItem, QSpinBox,
+    QVBoxLayout, QWidget)
 
 class Ui_formAusruestung(object):
     def setupUi(self, formAusruestung):
@@ -28,15 +28,24 @@ class Ui_formAusruestung(object):
         formAusruestung.setMinimumSize(QSize(802, 0))
         self.verticalLayout = QVBoxLayout(formAusruestung)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(20, 20, 20, 20)
-        self.gbRstungen = QGroupBox(formAusruestung)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.scrollArea = QScrollArea(formAusruestung)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 969, 733))
+        self.verticalLayout_2 = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(20, 20, 20, 20)
+        self.gbRstungen = QGroupBox(self.scrollAreaWidgetContents)
         self.gbRstungen.setObjectName(u"gbRstungen")
         self.verticalLayout_3 = QVBoxLayout(self.gbRstungen)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.verticalLayout_3.setContentsMargins(20, 20, 20, 20)
         self.Ruestungen = QGridLayout()
         self.Ruestungen.setObjectName(u"Ruestungen")
-        self.Ruestungen.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.Ruestungen.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
         self.labelRarm = QLabel(self.gbRstungen)
         self.labelRarm.setObjectName(u"labelRarm")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
@@ -48,16 +57,16 @@ class Ui_formAusruestung(object):
         font = QFont()
         font.setBold(True)
         self.labelRarm.setFont(font)
-        self.labelRarm.setAlignment(Qt.AlignCenter)
+        self.labelRarm.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelRarm, 0, 6, 1, 1)
 
         self.spinGesamtBauch = QSpinBox(self.gbRstungen)
         self.spinGesamtBauch.setObjectName(u"spinGesamtBauch")
         self.spinGesamtBauch.setMinimumSize(QSize(44, 0))
-        self.spinGesamtBauch.setAlignment(Qt.AlignCenter)
+        self.spinGesamtBauch.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtBauch.setReadOnly(True)
-        self.spinGesamtBauch.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtBauch.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtBauch.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtBauch, 1, 7, 1, 1)
@@ -65,9 +74,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtKopf = QSpinBox(self.gbRstungen)
         self.spinGesamtKopf.setObjectName(u"spinGesamtKopf")
         self.spinGesamtKopf.setMinimumSize(QSize(44, 0))
-        self.spinGesamtKopf.setAlignment(Qt.AlignCenter)
+        self.spinGesamtKopf.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtKopf.setReadOnly(True)
-        self.spinGesamtKopf.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtKopf.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtKopf.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtKopf, 1, 9, 1, 1)
@@ -81,7 +90,7 @@ class Ui_formAusruestung(object):
         self.labelPunkte = QLabel(self.gbRstungen)
         self.labelPunkte.setObjectName(u"labelPunkte")
         self.labelPunkte.setFont(font)
-        self.labelPunkte.setAlignment(Qt.AlignCenter)
+        self.labelPunkte.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelPunkte, 0, 10, 1, 1)
 
@@ -93,9 +102,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtLarm = QSpinBox(self.gbRstungen)
         self.spinGesamtLarm.setObjectName(u"spinGesamtLarm")
         self.spinGesamtLarm.setMinimumSize(QSize(44, 0))
-        self.spinGesamtLarm.setAlignment(Qt.AlignCenter)
+        self.spinGesamtLarm.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtLarm.setReadOnly(True)
-        self.spinGesamtLarm.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtLarm.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtLarm.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtLarm, 1, 5, 1, 1)
@@ -103,7 +112,7 @@ class Ui_formAusruestung(object):
         self.labelRS = QLabel(self.gbRstungen)
         self.labelRS.setObjectName(u"labelRS")
         self.labelRS.setFont(font)
-        self.labelRS.setAlignment(Qt.AlignCenter)
+        self.labelRS.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelRS, 0, 3, 1, 1)
 
@@ -111,7 +120,7 @@ class Ui_formAusruestung(object):
         self.labelLarm.setObjectName(u"labelLarm")
         self.labelLarm.setMinimumSize(QSize(35, 0))
         self.labelLarm.setFont(font)
-        self.labelLarm.setAlignment(Qt.AlignCenter)
+        self.labelLarm.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelLarm, 0, 5, 1, 1)
 
@@ -119,9 +128,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtPunkte.setObjectName(u"spinGesamtPunkte")
         self.spinGesamtPunkte.setMinimumSize(QSize(44, 0))
         self.spinGesamtPunkte.setFrame(False)
-        self.spinGesamtPunkte.setAlignment(Qt.AlignCenter)
+        self.spinGesamtPunkte.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtPunkte.setReadOnly(True)
-        self.spinGesamtPunkte.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtPunkte.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtPunkte.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtPunkte, 1, 10, 1, 1)
@@ -130,7 +139,7 @@ class Ui_formAusruestung(object):
         self.labelKopf.setObjectName(u"labelKopf")
         self.labelKopf.setMinimumSize(QSize(35, 0))
         self.labelKopf.setFont(font)
-        self.labelKopf.setAlignment(Qt.AlignCenter)
+        self.labelKopf.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelKopf, 0, 9, 1, 1)
 
@@ -138,16 +147,16 @@ class Ui_formAusruestung(object):
         self.labelBE.setObjectName(u"labelBE")
         self.labelBE.setMinimumSize(QSize(35, 0))
         self.labelBE.setFont(font)
-        self.labelBE.setAlignment(Qt.AlignCenter)
+        self.labelBE.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelBE, 0, 2, 1, 1)
 
         self.spinGesamtBein = QSpinBox(self.gbRstungen)
         self.spinGesamtBein.setObjectName(u"spinGesamtBein")
         self.spinGesamtBein.setMinimumSize(QSize(44, 0))
-        self.spinGesamtBein.setAlignment(Qt.AlignCenter)
+        self.spinGesamtBein.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtBein.setReadOnly(True)
-        self.spinGesamtBein.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtBein.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtBein.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtBein, 1, 4, 1, 1)
@@ -160,9 +169,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtRS = QSpinBox(self.gbRstungen)
         self.spinGesamtRS.setObjectName(u"spinGesamtRS")
         self.spinGesamtRS.setMinimumSize(QSize(44, 0))
-        self.spinGesamtRS.setAlignment(Qt.AlignCenter)
+        self.spinGesamtRS.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtRS.setReadOnly(True)
-        self.spinGesamtRS.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtRS.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtRS.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtRS, 1, 3, 1, 1)
@@ -170,9 +179,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtBrust = QSpinBox(self.gbRstungen)
         self.spinGesamtBrust.setObjectName(u"spinGesamtBrust")
         self.spinGesamtBrust.setMinimumSize(QSize(44, 0))
-        self.spinGesamtBrust.setAlignment(Qt.AlignCenter)
+        self.spinGesamtBrust.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtBrust.setReadOnly(True)
-        self.spinGesamtBrust.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtBrust.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtBrust.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtBrust, 1, 8, 1, 1)
@@ -180,9 +189,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtBE = QSpinBox(self.gbRstungen)
         self.spinGesamtBE.setObjectName(u"spinGesamtBE")
         self.spinGesamtBE.setMinimumSize(QSize(44, 0))
-        self.spinGesamtBE.setAlignment(Qt.AlignCenter)
+        self.spinGesamtBE.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtBE.setReadOnly(False)
-        self.spinGesamtBE.setButtonSymbols(QAbstractSpinBox.PlusMinus)
+        self.spinGesamtBE.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.PlusMinus)
         self.spinGesamtBE.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtBE, 1, 2, 1, 1)
@@ -193,7 +202,7 @@ class Ui_formAusruestung(object):
         self.labelBauch.setSizePolicy(sizePolicy)
         self.labelBauch.setMinimumSize(QSize(35, 0))
         self.labelBauch.setFont(font)
-        self.labelBauch.setAlignment(Qt.AlignCenter)
+        self.labelBauch.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelBauch, 0, 7, 1, 1)
 
@@ -201,7 +210,7 @@ class Ui_formAusruestung(object):
         self.labelBrust.setObjectName(u"labelBrust")
         self.labelBrust.setMinimumSize(QSize(35, 0))
         self.labelBrust.setFont(font)
-        self.labelBrust.setAlignment(Qt.AlignCenter)
+        self.labelBrust.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelBrust, 0, 8, 1, 1)
 
@@ -209,7 +218,7 @@ class Ui_formAusruestung(object):
         self.labelBein.setObjectName(u"labelBein")
         self.labelBein.setMinimumSize(QSize(35, 0))
         self.labelBein.setFont(font)
-        self.labelBein.setAlignment(Qt.AlignCenter)
+        self.labelBein.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.Ruestungen.addWidget(self.labelBein, 0, 4, 1, 1)
 
@@ -221,9 +230,9 @@ class Ui_formAusruestung(object):
         self.spinGesamtRarm = QSpinBox(self.gbRstungen)
         self.spinGesamtRarm.setObjectName(u"spinGesamtRarm")
         self.spinGesamtRarm.setMinimumSize(QSize(44, 0))
-        self.spinGesamtRarm.setAlignment(Qt.AlignCenter)
+        self.spinGesamtRarm.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.spinGesamtRarm.setReadOnly(True)
-        self.spinGesamtRarm.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.spinGesamtRarm.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
         self.spinGesamtRarm.setMaximum(999)
 
         self.Ruestungen.addWidget(self.spinGesamtRarm, 1, 6, 1, 1)
@@ -240,23 +249,27 @@ class Ui_formAusruestung(object):
         self.checkZonen = QCheckBox(self.gbRstungen)
         self.checkZonen.setObjectName(u"checkZonen")
         self.checkZonen.setMinimumSize(QSize(0, 18))
-        self.checkZonen.setLayoutDirection(Qt.RightToLeft)
+        self.checkZonen.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         self.checkZonen.setChecked(True)
 
         self.verticalLayout_3.addWidget(self.checkZonen)
 
         self.labelHinweis = QLabel(self.gbRstungen)
         self.labelHinweis.setObjectName(u"labelHinweis")
-        self.labelHinweis.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        self.labelHinweis.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
 
         self.verticalLayout_3.addWidget(self.labelHinweis)
 
+        self.verticalSpacer_4 = QSpacerItem(20, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout.addWidget(self.gbRstungen)
+        self.verticalLayout_3.addItem(self.verticalSpacer_4)
 
-        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout.addItem(self.verticalSpacer_4)
+        self.verticalLayout_2.addWidget(self.gbRstungen)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verticalLayout.addWidget(self.scrollArea)
 
 
         self.retranslateUi(formAusruestung)
@@ -266,6 +279,7 @@ class Ui_formAusruestung(object):
 
     def retranslateUi(self, formAusruestung):
         formAusruestung.setWindowTitle(QCoreApplication.translate("formAusruestung", u"Form", None))
+        self.scrollArea.setProperty(u"class", QCoreApplication.translate("formAusruestung", u"transparent", None))
         self.gbRstungen.setTitle("")
         self.labelRarm.setText(QCoreApplication.translate("formAusruestung", u"R. Arm", None))
         self.labelRarm.setProperty(u"class", QCoreApplication.translate("formAusruestung", u"h4", None))
