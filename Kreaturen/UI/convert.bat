e3dce6140889e68c16cad0b42f42e1a3990cd8a5
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2025-02-15 10:03:40 +0100

    Update ui convert scripts to use venv. Important: we want to use the Sephrasto venv, for that we expect the sephrasto repository to be in the same folder as the plugin repo.

--- Kreaturen/UI/convert.bat
+++ Kreaturen/UI/convert.bat
@@ -3,8 +3,8 @@
 cd /d %0\..
 
 for /r %%f in (*.ui) do (
-    ECHO pyside6-uic.exe %%~nf.ui -o ../%%~nf.py
-    CALL pyside6-uic.exe %%~nf.ui -o ../%%~nf.py
+    ECHO ..\..\..\Sephrasto\venv\Scripts\pyside6-uic.exe %%~nf.ui -o ../%%~nf.py
+    CALL ..\..\..\Sephrasto\venv\Scripts\pyside6-uic.exe %%~nf.ui -o ../%%~nf.py
 )
 
 PAUSE
