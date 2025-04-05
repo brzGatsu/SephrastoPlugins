c4699333c3a2a3071c6d8257ba105a4e19c6a1cf
brzGatsu <1515649+brzGatsu@users.noreply.github.com>
2025-04-05 14:40:28 +0200

    FertigkeitenPlus Plugin: Remove feature to allow specifying a fourth attribute for ferts

--- FertigkeitenPlus/__init__.py
+++ FertigkeitenPlus/__init__.py
@@ -44,9 +44,6 @@ Talent._updateAnzeigenameExt = updateAnzeigeName
 class Plugin:
     def __init__(self):
         EventBus.addAction("datenbank_laden", self.datenbankLadenHook)
-        EventBus.addAction("basisdatenbank_geladen", self.basisDatenbankGeladenHook)
-        EventBus.addFilter("dbe_class_fertigkeitdefinition_wrapper", self.dbeClassFertigkeitFilter)
-        EventBus.addFilter("dbe_class_ueberfertigkeitdefinition_wrapper", self.dbeClassFertigkeitFilter)
         EventBus.addFilter("class_talentpicker_wrapper", self.talentPickerFilter)
         EventBus.addAction("talent_serialisiert", self.talentSerialisiertHook)
         EventBus.addAction("talent_deserialisiert", self.talentDeserialisiertHook)
@@ -76,10 +73,6 @@ class Plugin:
         e.text = "0.5"
         e.typ = "Float"
         self.db.loadElement(e)
-
-    def basisDatenbankGeladenHook(self, params):
-        e = self.db.einstellungen["Fertigkeiten: BW Script"]
-        e.text = "round(sum(sorted(getAttribute(), reverse=True)[:3])/3)"
 
     def talentSerialisiertHook(self, params):
         ser = params["serializer"]
@@ -185,39 +178,3 @@ class Plugin:
                 self.updateErfahrungsgradLabels()
 
         return FertigkeitenPlusTalentPickerWrapper
-
-    def dbeClassFertigkeitFilter(self, editorType, params):
-        class DatenbankEditFertigkeitWrapperPlus(editorType):
-            def __init__(self, datenbank, fertigkeit=None):
-                super().__init__(datenbank, fertigkeit)
-
-            def onSetupUi(self):
-                super().onSetupUi()
-                self.ui.comboAttribut4Separator = QtWidgets.QLabel()
-                self.ui.comboAttribut4Separator.setText(" Optional:")
-                self.ui.comboAttribut4 = QtWidgets.QComboBox()
-                self.ui.comboAttribut4.setMinimumSize(QtCore.QSize(45, 0))
-                attribute = [a.name for a in sorted(self.datenbank.attribute.values(), key=lambda value: value.sortorder)]
-                self.ui.comboAttribut4.addItems(["-"] + attribute)
-                self.ui.horizontalLayout.insertWidget(5, self.ui.comboAttribut4Separator)
-                self.ui.horizontalLayout.insertWidget(6, self.ui.comboAttribut4)
-                
-                self.registerInput(self.ui.comboAttribut4, self.ui.labelAttribute)
-
-            def load(self, fertigkeit):
-                super().load(fertigkeit)
-                if len(fertigkeit.attribute) >= 4:
-                    self.ui.comboAttribut4.setCurrentText(fertigkeit.attribute[3])
-
-            def update(self, fertigkeit):
-                super().update(fertigkeit)
-                if self.ui.comboAttribut4.currentText() == "-":
-                    if len(fertigkeit.attribute) >= 4:
-                        fertigkeit.attribute.pop()
-                else:
-                    if len(fertigkeit.attribute) < 4:
-                        fertigkeit.attribute.append(self.ui.comboAttribut4.currentText())
-                    else:
-                        fertigkeit.attribute[3] = self.ui.comboAttribut4.currentText()
-
-        return DatenbankEditFertigkeitWrapperPlus
